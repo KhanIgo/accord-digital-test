e088897daf1e4095add1b2f56ac2bbe6ce902867
Igor' Khan <igor.swit4@gmail.com>
2019-04-14 12:03:53 +1000

    font added

--- build/assets/js/responsive.js
+++ build/assets/js/responsive.js
@@ -1,24 +1,23 @@
-jQuery(function($){
-    var body = $('body');
+let viewportW, viewportH;
+jQuery( $ => {
+    let body = $('body');
     detect_device_type();
 
     $( window ).load( function(){
-        w = $( window ).width();
+        viewportW = $( window ).width();
+        viewportH = $( window ).height();
     });
-    
-    $(window).resize(function(){
-        
-        if( w != $( window ).width() ){
-            detect_viewport_orientation();
-            //set_viewport();
-            w = $( window ).width();
 
-          }
-    });
-    
+    // $(window).resize(function(){
+    //     if( viewportW != $( window ).width() ){
+    //         detect_viewport_orientation();
+    //         viewportW = $( window ).width();
+    //       }
+    // });
+
     function detect_device_type(){
         detect_viewport_orientation();
-        
+
         md = new MobileDetect(window.navigator.userAgent);
         var phone = md.phone();
         var tablet = md.tablet();
@@ -29,7 +28,7 @@ jQuery(function($){
         if(tablet) device_type = 'tablet';
         body = $('body');
         body.addClass(device_type);
-        set_viewport();
+        // set_viewport();
     }
     function set_viewport(){
         if( device_type=='tablet' && viewport_orientation=='landscape'){
@@ -46,13 +45,12 @@ jQuery(function($){
             var scale = vWidth/480;
             viewport_content = viewport_content+ ', initial-scale=' +scale;
             //viewport_content = viewport_content+ ', initial-scale=1';
-            console.log(vWidth);
         }
         //viewport_content += ', minimum-scale=1, maximum-scale=1, initial-scale=1, user-scalable=no';
-        
+
         if( device_type=='phone' && viewport_orientation=='portrait'){}
         else{ viewport_content += ', initial-scale=1'; }
-        
+
         //console.log('viewport_content: '+viewport_content);
         //$('#viewport-meta').attr('content', viewport_content);
         $('meta[name=viewport]').attr('content', viewport_content);
@@ -60,7 +58,7 @@ jQuery(function($){
     function detect_viewport_orientation(){
         vWidth = window.innerWidth;
         vHeight = window.innerHeight;
-        
+
         if(vWidth > vHeight) viewport_orientation = 'landscape';
         else if(vWidth < vHeight) viewport_orientation = 'portrait';
         body = $('body');
@@ -71,21 +69,5 @@ jQuery(function($){
             body.addClass('portrait').removeClass('landscape');
         }
     }
-    
-});
-window.onload = function () {
-    hideAddressBar();
-    window.addEventListener("orientationchange", function () {
-        hideAddressBar();
-    }, false);
-}
 
-function hideAddressBar() {
-    setTimeout(function () {
-        document.body.style.height = window.outerHeight + 'px';
-        setTimeout(function () {
-            //window.scrollTo(0, 1);
-        }, 1100);
-    }, 1000);
-    return false;
-}
+});
