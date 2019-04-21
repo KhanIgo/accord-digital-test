ac3e280f6b9482c32d373d891f4a9af821339d25
Igor' Khan <igor.swit4@gmail.com>
2019-04-21 20:20:32 +1000

    php

--- src/php_templates/_b-main.php
+++ src/php_templates/_b-main.php
@@ -1,7 +1,11 @@
 <section class="main row">
     <div class="main__container container">
         <div class="main__photo-and-sub">
-            <div class="main__photo-wrpr"><img class="main__photo" data-src="images/Pack.png" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="" role="presentation" /><img class="main__shadow" data-src="images/shadow.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><img class="main__plants-1" data-src="images/plants-1.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /><img class="main__plants-2" data-src="images/plants-2.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /> </div>
+            <div class="main__photo-wrpr">
+                <img class="main__photo" data-src="images/Pack.png" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="" role="presentation" />
+                <img class="main__shadow" data-src="images/shadow.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+                <img class="main__plants-1" data-src="images/plants-1.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+                <img class="main__plants-2" data-src="images/plants-2.png" alt="" role="presentation" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /> </div>
             <div class="main__sub">
                 <p>
                     <?= $data['description_text']; ?>
